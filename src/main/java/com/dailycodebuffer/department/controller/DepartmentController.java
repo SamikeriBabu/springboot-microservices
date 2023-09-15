72104ee68612e9819ef6a03d687c443c64aa40c0
Babu <samikeribabu@gmail.com>
2023-09-15 14:29:19 +0530

    first commit - microservices

---
+++ src/main/java/com/dailycodebuffer/department/controller/DepartmentController.java
@@ -0,0 +1,19 @@
+package com.dailycodebuffer.department.controller;
+
+import com.dailycodebuffer.department.entity.Department;
+import com.dailycodebuffer.department.service.DepartmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/departments")
+public class DepartmentController {
+    @Autowired
+    private DepartmentService departmentService;
+    @PostMapping("/")
+    public Department saveDepartment(Department department){
+        return DepartmentService.saveDepartment(department);
+    }
+}
---
+++ src/main/java/com/dailycodebuffer/department/service/DepartmentService.java
@@ -0,0 +1,16 @@
+package com.dailycodebuffer.department.service;
+
+import com.dailycodebuffer.department.entity.Department;
+import com.dailycodebuffer.department.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DepartmentService {
+    @Autowired
+    private static DepartmentRepository departmentRepository;
+
+    public Department saveDepartment(Department department) {
+        return departmentRepository.save(department);
+    }
+}
